3ccf0f4e13220f5c3640f6454db694f8f58a60f0
abderrahim ghmouch <abderrahimghmouch47@gmail.com>
2026-04-12 18:08:47 +0100

    i fixed the json status in admin midelware

--- app/Http/Middleware/AdminMiddleware.php
+++ app/Http/Middleware/AdminMiddleware.php
@@ -11,17 +11,17 @@ class AdminMiddleware
     /**
      * Handle an incoming request.
      *
-     * @param  \Closure(\Illuminate\Http\Request): 
+     * @param  \Closure(\Illuminate\Http\Request):
      */
     public function handle(Request $request, Closure $next): Response
     {
-    
+
         if (auth('api')->check() && auth('api')->user()->role === 'admin') {
             return $next($request);
         }
         else {
 
-        return response()->json(['message'=>'unauthorized admin'], ['status'=>'eror]',403]);
+        return response()->json(['message'=>'unauthorized admin'],403,['status'=>'error']);
           }
     }
 }
