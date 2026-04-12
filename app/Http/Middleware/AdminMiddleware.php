76d353fbe9088900a37376a7b083caf9cb231c91
abderrahim ghmouch <abderrahimghmouch47@gmail.com>
2026-04-12 18:03:58 +0100

    implementing jwt authentification system and create the middelware of the porter

--- app/Http/Middleware/AdminMiddleware.php
+++ app/Http/Middleware/AdminMiddleware.php
@@ -11,19 +11,17 @@ class AdminMiddleware
     /**
      * Handle an incoming request.
      *
-     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     * @param  \Closure(\Illuminate\Http\Request): 
      */
     public function handle(Request $request, Closure $next): Response
     {
-        // 1. Check if user is logged in AND has admin role
+    
         if (auth('api')->check() && auth('api')->user()->role === 'admin') {
             return $next($request);
         }
+        else {
 
-        // 2. Return proper JSON response for API
-        return response()->json([
-            'status' => 'error',
-            'message' => 'Unauthorized. Admin access required.'
-        ], 403);
+        return response()->json(['message'=>'unauthorized admin'], ['status'=>'eror]',403]);
+          }
     }
 }
